1ef03571bf51e086d852adb201df51ff52cb5cc8
Nongdada <ronnachai_si@cmu.ac.th>
2023-03-03 15:39:46 +0700

    huh

--- average.cpp
+++ average.cpp
@@ -1,1 +1,21 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std;
 
+int main(int huh,char *huhh[]){
+        if(huh > 1){
+             float a = 0 ;
+            for(int i = 0; i < huh-1; i ++)
+    {
+         a+=atof(huhh[i+1]);
+    }                
+    cout <<"---------------------------------" << endl;
+         cout << "Average of "<< huh -1<<" numbers = " << a/(huh-1)<< endl;
+         cout <<"---------------------------------" << endl;
+
+        }else{
+             cout << "Please input numbers to find average.\n";
+        }
+
+    return 0;
+}
